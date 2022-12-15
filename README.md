80f2cbca897c87d3c0e46e5a6a4f1ffe76e514c7
Wesley <we95sz@gmail.com>
2022-12-15 13:57:42 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# nginx
+Configurações para Nginx
+
+#Permissão:
+	chmod 777
+
+#Ansible: 
+	centos7_ansible_install = Ansible scripts para Centos 7
+	ubuntu_ansible_install = Ansible scripts for Ubuntu 20.04 LTS
+
+#Shell Script:
+	centos7_install_nginx.sh = Install para Centos 7
+	ubuntu_install_nginx.sh = Install para Ubuntu 20.04 LTS
+
+
+#Configurações
+ 	etc_nginx = Configurações padrões para /etc/nginx 
---
+++ ubuntu_install_nginx.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#Wesley Duarte
+#https://github.com/95wesley
+
+echo "#####################################"
+echo "# Instalando pré-requisitos com APT #"
+echo -e "####################################\n\n"
+
+apt install -y  curl gnupg2 ca-certificates lsb-release ubuntu-keyring
+
+echo -e "\n\n"
+echo "###################################################"
+echo "# Importando chave de assinatura oficial do nginx #"
+echo -e "##################################################\n\n"
+
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+
+echo -e "\n\n"
+echo "############################################################"
+echo "# Verificando se o arquivo baixado contém a chave adequada #"
+echo -e "###########################################################\n\n"
+
+mkdir /root/.gnupg/
+gpg --dry-run --quiet --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
+
+echo -e "\n\n"
+echo "##################################"
+echo "# Adicionando Repositório Stable #"
+echo -e "##################################\n\n"
+
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
+
+
+echo -e "\n\n"
+echo "####################"
+echo "# Instalando Nginx #"
+echo -e "####################\n\n"
+
+apt update
+apt install nginx
+
+echo -e "\n\n"
+echo "###############"
+echo "# Finalizando #"
+echo -e "###############\n\n"
